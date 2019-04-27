c2b68c97e68346e223e3d15d708340f65388f8a9
Ryan Chu <tianyuan.chu@gmail.com>
2019-04-27 11:20:08 -0700

    react-redux

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { connect } from "react-redux";
+import { toggleTodo } from "../redux/actions";
+
+const Todo = ({ todo, toggleTodo }) => (
+    <li className="todo-item" onClick={() => toggleTodo(todo.id)}>
+        {todo && todo.completed ? "👌" : "👋"}{" "}
+        <span className={"todo-item__text"}>{todo.content}</span>
+    </li>
+);
+
+// export default Todo;
+export default connect(
+    null,
+    { toggleTodo }
+)(Todo);
