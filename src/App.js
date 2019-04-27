e8d9bceb0fe8dd80376fb52355019736d15349ea
Ryan Chu <tianyuan.chu@gmail.com>
2019-04-27 11:34:38 -0700

    default export

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import AddTodo from "./components/AddTodo";
-import TodoList from "./components/TodoList";
-import VisibilityFilters from "./components/VisibilityFilters";
+import { AddTodo, TodoList, VisibilityFilters } from "./components";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <div className="App-header">
+            <header className="App-header">
                 <h1>Todo List</h1>
                 <AddTodo />
                 <TodoList />
                 <VisibilityFilters />
-            </div>
+            </header>
         </div>
     );
 }
