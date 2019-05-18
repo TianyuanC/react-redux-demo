f8a77534fe1762beadaf096833562ea90f035007
Ryan Chu <tianyuan.chu@gmail.com>
2019-05-18 14:32:55 -0700

    multi todo lists

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { AddTodo, TodoList, VisibilityFilters, UndoRedo } from "./components";
 import "./App.css";
+import { Provider } from "react-redux";
+import initStore from "./redux/store";
 
 const TodoApp = ({ note }) => (
-    <>
+    <Provider store={initStore()}>
         <h1>Todo List {note}</h1>
         <AddTodo />
         <TodoList />
         <VisibilityFilters />
         <UndoRedo />
-    </>
+    </Provider>
 );
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <TodoApp />
+                <TodoApp note={1} />
+                <TodoApp note={2} />
+                <TodoApp note={3} />
             </header>
         </div>
     );
